7f519cd5791bb0039ca4c7d4dcecaff598577671
“Egor” <naunecontrol@yandex.ru>
2024-02-06 07:11:48 +0300

    msg11

--- routes/admin.js
+++ routes/admin.js
@@ -5,11 +5,6 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const jsonParser = express.json();
 
-<<<<<<< HEAD
-// комментарий из ветки моделей
-=======
-// контроллеры для админ-панели
->>>>>>> 9109981a93410ebbf49cdd36665497101267b646
 router.get('/', adminController.getArticleAll);
 router.get('/:idArticle', adminController.getArticle)
 router.post('/addArticle', urlencodedParser, adminController.addArticle);
